505a8f1498b45ffb50cf24d2a54977641ae6d517
numberlock <numberlock@gmail.com>
2022-10-08 00:59:31 +0200

    working on displayTask, cleaned up some of the code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import "./style.css";
 const btnOpenProject = document.querySelector(".show-add-project");
 const btnOpenTask = document.querySelector(".show-add-task");
 const btnNewTask = document.querySelector(".btn-add-task");
-const inputNewProject = document.querySelector("#input-new-project");
 const content = document.querySelector(".content ");
 const taskOverlay = document.querySelector(".task-overlay");
 const projectOverlay = document.querySelector(".project-overlay");
@@ -26,18 +25,18 @@ const projectFactory = function (newName) {
 function projectController() {
   function createProject(projectName) {
     let project = projectFactory(projectName);
-    toStorage(project.name, project.task);
+    toStorage(project.name, project);
   }
 
   function toStorage(projectName, obj) {
-    window.localStorage.setItem(projectName, obj);
+    window.localStorage.setItem(projectName, JSON.stringify(obj));
   }
 
   function removeProject(projectName) {
     window.localStorage.removeItem(projectName);
   }
 
-  return { createProject, removeProject };
+  return { createProject, removeProject, toStorage };
 }
 
 //task
@@ -52,13 +51,147 @@ const taskFactory = function (
   let taskTitle = inputTitle;
   let description = inputDesc;
   let dueDate = inputDate;
-  let property = inputPriority;
+  let priority = inputPriority;
   let notes = inputNotes;
 
-  return { taskTitle, description, dueDate, property, notes };
+  return { taskTitle, description, dueDate, priority, notes };
 };
 
-const taskController = function () {};
+btnOpenTask.addEventListener("click", () => {
+  taskOverlay.style.display = "block";
+});
+
+const taskController = function () {
+  function getProject() {
+    const selectedProject = document.querySelector(".active");
+    const projectName = selectedProject.textContent;
+
+    return JSON.parse(window.localStorage.getItem(projectName));
+  }
+
+  function createTask(
+    inputTitle,
+    inputDesc,
+    inputDate,
+    inputPriority,
+    inputNotes
+  ) {
+    let project = getProject();
+    let tasks = taskFactory(
+      inputTitle.value,
+      inputDesc.value,
+      inputDate.value,
+      inputPriority.value,
+      inputNotes.value
+    );
+    project.task.push(tasks);
+
+    toStorage(project.name, project);
+  }
+
+  function updateTask(
+    inputTitle,
+    inputDesc,
+    inputDate,
+    inputPriority,
+    inputNotes
+  ) {
+    let project = getProject();
+    let projectTasks = project.task[index];
+
+    projectTasks.taskName = inputTitle.value;
+    projectTasks.description = inputDesc.value;
+    projectTasks.dueDate = inputDate.value;
+    projectTasks.priority = inputPriority.value;
+    projectTasks.notes = inputNotes.value;
+    projectTasks.taskName = inputTitle.value;
+
+    toStorage(project.name, project);
+  }
+
+  function toStorage(projectName, obj) {
+    window.localStorage.setItem(projectName, JSON.stringify(obj));
+  }
+
+  function deleteTask(index) {
+    let project = getProject();
+    project.task.splice(Number(index), 1);
+
+    toStorage(project.name, project);
+  }
+
+  return { createTask, deleteTask, updateTask, toStorage };
+};
+
+btnNewTask.addEventListener("click", () => {
+  taskController().createTask(
+    inputTitle,
+    inputDesc,
+    inputDate,
+    inputPriority,
+    inputNotes
+  );
+
+  taskOverlay.style.display = "none";
+});
+
+function displayTasks() {
+  const selectedProject = document.querySelector(".active");
+  const projectName = selectedProject.textContent;
+  let localProject = JSON.parse(window.localStorage.getItem(projectName));
+  let tasks = localProject.task;
+
+  function generateTasks(name, desc, date, priority, notes, index) {
+    taskController.toStorage();
+    const tasksContainer = document.querySelector(".tasks");
+
+    const removeFromLocalArray = function () {
+      tasks.splice(Number(index), 1);
+
+      projectController().toStorage(projectName, localProject);
+    };
+
+    const divContainer = document.createElement("div");
+    const divName = document.createElement("div");
+    divName.textContent = name;
+    const divDesc = document.createElement("div");
+    divDesc.textContent = desc;
+    const divDate = document.createElement("div");
+    divDate.textContent = date;
+    const divPriority = document.createElement("div");
+    divPriority.textContent = priority;
+    const divNotes = document.createElement("div");
+    divNotes.textContent = notes;
+    const divIcons = document.createElement("div");
+    const editTask = document.createElement("i");
+    editTask.classList.add("glyphicon", "glyphicon-pencil");
+    const removeTask = document.createElement("i");
+    removeTask.classList.add("glyphicon", "glyphicon-remove");
+
+    removeTask.addEventListener("click", removeFromLocalArray);
+
+    tasksContainer.appendChild(divContainer);
+    divContainer.append(
+      divName,
+      divDesc,
+      divDate,
+      divPriority,
+      divNotes,
+      divIcons
+    );
+    divIcons.append(editTask, removeTask);
+  }
+
+  for (let i = 0; i < tasks.length; i++) {
+    const name = tasks[i].taskTitle;
+    const desc = tasks[i].description;
+    const date = tasks[i].dueDate;
+    const priority = tasks[i].priority;
+    const notes = tasks[i].notes;
+
+    generateTasks(name, desc, date, priority, notes, i);
+  }
+}
 
 // submit project
 
@@ -85,9 +218,12 @@ function displayProject(name) {
   let projectName = document.createElement("div");
   projectName.classList.add("project-name");
   projectName.textContent = name;
-  let projectRemove = document.createElement("div");
-  projectRemove.classList.add("project-remove");
-  projectRemove.textContent = "🗑️";
+  let projectRemove = document.createElement("i");
+  projectRemove.classList.add(
+    "project-remove",
+    "glyphicon",
+    "glyphicon-remove"
+  );
 
   removeProject(projectRemove, projectContainer, name);
   projectContainer.append(projectName, projectRemove);
@@ -109,60 +245,7 @@ function activeProject() {
         element.classList.remove("active");
       });
       project.classList.add("active");
-    });
-  });
-}
-
-//submit task
-
-/* let displayTasks = function () {
-  let storage = localStorage.getItem(active);
-  const displayDiv = document.querySelector(".tasks");
-  displayDiv.textContent = storage;
-};
- */
-
-btnOpenTask.addEventListener("click", () => {
-  taskOverlay.style.display = "block";
-});
-/* 
-btnNewProject.addEventListener("click", () => {
-  projectController.createProject(inputNewProject.value);
-  projectOverlay.style.display = "none";
-});
-
-btnNewTask.addEventListener("click", () => {
-  taskOverlay.style.display = "none";
-  toStorage(
-    taskFactory(inputTitle, inputDesc, inputDate, inputPriority, inputNotes)
-  );
-  displayTasks();
-});
-
-const makeProject = (name) => {
-  let createProject = document.createElement("div");
-  createProject.classList.add("project");
-  createProject.textContent = name;
-  content.appendChild(createProject);
-};
-
-let active = "";
-function activeProject() {
-  const allProjects = document.querySelectorAll(".project");
-  allProjects.forEach((project) => {
-    project.addEventListener("click", () => {
-      active = project.name;
       displayTasks();
     });
   });
 }
-
-function toStorage(object) {
-  let storage = localStorage.getItem(active);
-  if (storage == null) {
-    localStorage.setItem(active, JSON.stringify(object));
-  } else {
-    localStorage.setItem(active, storage + JSON.stringify(object));
-  }
-}
- */
