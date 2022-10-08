fdcbbdf1ca42f0dbc85b05d6c4859a9d58981543
numberlock <numberlock@gmail.com>
2022-10-08 10:00:35 +0200

    working on edit task button

--- src/index.js
+++ src/index.js
@@ -131,9 +131,95 @@ btnNewTask.addEventListener("click", () => {
     inputPriority,
     inputNotes
   );
+  clearTaks();
+  displayTasks();
 
   taskOverlay.style.display = "none";
 });
+
+function clearTaks() {
+  let container = document.querySelector(".tasks");
+  while (container.hasChildNodes()) {
+    container.removeChild(container.lastChild);
+  }
+}
+
+function editTasks() {
+  const htmlDiv = document.querySelector(".edit-task");
+
+  //form
+  const editTaskOverlay = document.createElement("div");
+  editTaskOverlay.classList.add("edit-task-overlay");
+  editTaskOverlay.style.display = "block";
+
+  const editTaskModel = document.createElement("div");
+  editTaskModel.classList.add("edit-task-model");
+
+  //object
+
+  const labelForName = document.createElement("label");
+  labelForName.for = "edit-task-title";
+  labelForName.textContent = "Task Title:";
+  const editInputName = document.createElement("input");
+  editInputName.id = "edit-task-title";
+
+  const labelForDesc = document.createElement("label");
+  labelForDesc.for = "edit-task-desc";
+  labelForDesc.textContent = "Description:";
+  const editInputDesc = document.createElement("input");
+  editInputDesc.id = "edit-task-desc";
+
+  const labelForDate = document.createElement("label");
+  labelForDate.for = "edit-task-date";
+  labelForDate.textContent = "Due Date:";
+  const editInputDate = document.createElement("input");
+  editInputDate.type = "date";
+  editInputDesc.id = "edit-task-date";
+
+  const labelForPrio = document.createElement("label");
+  labelForPrio.for = "edit-task-prio";
+  labelForPrio.textContent = "Task Priority:";
+  const editInputPrio = document.createElement("select");
+  editInputDesc.id = "edit-task-prio";
+  const optionHigh = document.createElement("option");
+  optionHigh.value = "high";
+  optionHigh.textContent = "High";
+  const optionMedium = document.createElement("option");
+  optionMedium.value = "medium";
+  optionMedium.textContent = "Medium";
+  const optionLow = document.createElement("option");
+  optionLow.value = "low";
+  optionLow.textContent = "Low";
+
+  const labelForNotes = document.createElement("label");
+  labelForNotes.for = "edit-task-notes";
+  labelForNotes.textContent = "Notes:";
+
+  const editInputNotes = document.createElement("input");
+  editInputNotes.id = "edit-task-notes";
+
+  const editButton = document.createElement("button");
+  editButton.textContent = "Change";
+  editButton.classList.add("editButtonSubmit");
+
+  //append
+  htmlDiv.appendChild(editTaskOverlay);
+  editTaskOverlay.appendChild(editTaskModel);
+  editInputPrio.append(optionHigh, optionMedium, optionLow);
+  editTaskModel.append(
+    labelForName,
+    editInputName,
+    labelForDesc,
+    editInputDesc,
+    labelForDate,
+    editInputDate,
+    labelForPrio,
+    editInputPrio,
+    labelForNotes,
+    editInputNotes,
+    editButton
+  );
+}
 
 function displayTasks() {
   const selectedProject = document.querySelector(".active");
@@ -147,26 +233,31 @@ function displayTasks() {
     const removeFromLocalArray = function () {
       tasks.splice(Number(index), 1);
       taskController.toStorage(projectName, localProject);
+      clearTaks();
+      displayTasks();
     };
 
     const divContainer = document.createElement("div");
+    divContainer.classList.add(`indi-task-container`);
     const divName = document.createElement("div");
-    divName.textContent = name;
+    divName.textContent = `Task Title: ${name}`;
     const divDesc = document.createElement("div");
-    divDesc.textContent = desc;
+    divDesc.textContent = `Description: ${desc}`;
     const divDate = document.createElement("div");
-    divDate.textContent = date;
+    divDate.textContent = `Due Date: ${date}`;
     const divPriority = document.createElement("div");
-    divPriority.textContent = priority;
+    divPriority.textContent = `Priority: ${priority}`;
     const divNotes = document.createElement("div");
-    divNotes.textContent = notes;
+    divNotes.textContent = `Notes: ${notes}`;
     const divIcons = document.createElement("div");
+    divIcons.classList.add("task-icons");
     const editTask = document.createElement("i");
     editTask.classList.add("glyphicon", "glyphicon-pencil");
     const removeTask = document.createElement("i");
     removeTask.classList.add("glyphicon", "glyphicon-remove");
 
     removeTask.addEventListener("click", removeFromLocalArray);
+    editTask.addEventListener("click", editTasks);
 
     tasksContainer.appendChild(divContainer);
     divContainer.append(
@@ -243,7 +334,6 @@ function activeProject() {
         element.classList.remove("active");
       });
       project.classList.add("active");
-      displayTasks();
     });
   });
 }
